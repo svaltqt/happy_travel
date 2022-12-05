502f47a94a955f7179cf1bbf4e0143c29bbb6265
svaltqt <naxxudea@gmail.com>
2022-12-05 02:12:49 -0500

    verificar registro

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,47 @@
 $(document).ready(function(){
 
-    
+    function login() {
+
+        var usuario = $("#Rusuario").val();
+        var correo = $("#Rcorreo").val(); 
+        var contraseña = $("#Rcontraseña1").val(); 
+        var contraseña2 = $("#Rcontraseña2").val();
+        var tamañoPass = contraseña.length; 
+
+       if (tamañoPass < 8) {
+                var add = "<p>"                        
+                    add+="<center>"+"<p style='color:red;'>"+"Contraseña muy corta"+"</p>"+"</center>"                        
+                    add+="</p>"
+                    $("#Capturador").html(add);
+                }
+       else {
+        
+            if (contraseña == contraseña2) {
+                    return 1;                       
+                }
+            else {                    
+
+                    var add = "<p>"                        
+                    add+="<center>"+"<p style='color:red;'>"+"Contraseña no coincide"+"</p>"+"</center>"                        
+                    add+="</p>"
+                    $("#Capturador").html(add);
+                    return -1;
+                }
+        
+            }
 }
-);
+   
+   
+   
+    $("#registroUsuario").click(function(){
+        
+        login()
+            
 
         
+    })
+
+
+
+
+});
